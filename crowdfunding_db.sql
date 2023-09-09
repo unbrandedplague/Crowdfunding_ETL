247f270b9c0d6d32a1e0d58a459750be7b30ae53
marvina <anew9107life20xx@gmail.com>
2023-09-09 05:07:15 -0500

    finalML

--- crowdfunding_db.sql
+++ crowdfunding_db.sql
@@ -1,13 +1,31 @@
 -- Drop table if exists
 DROP TABLE IF EXISTS campaign;
 
-DROP TABLE IF EXISTS category;
+-- DROP TABLE IF EXISTS category;
 
-DROP TABLE IF EXISTS subcategory;
+-- DROP TABLE IF EXISTS subcategory;
 
-DROP TABLE IF EXISTS contacts;
+-- DROP TABLE IF EXISTS contacts;
 
 -- Create a new tables 
+
+CREATE TABLE category (
+	category_id VARCHAR (4) PRIMARY KEY,
+	category_name VARCHAR (15)
+);
+
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR (6) PRIMARY KEY,
+	subcategory_name VARCHAR (25)
+);
+
+CREATE TABLE contacts (
+	contact_id INT PRIMARY KEY,
+	first_name CHAR (100),
+	last_name CHAR (100),
+	email VARCHAR (255) 
+);
+
 CREATE TABLE campaign (
 	cf_id INT PRIMARY KEY,
 	contact_id INT NOT NULL,
@@ -24,25 +42,27 @@ CREATE TABLE campaign (
 	category VARCHAR (50) NOT NULL,
 	subcategory VARCHAR (50) NOT NULL,
 	category_id VARCHAR (4) NOT NULL,
-	subcategory_id VARCHAR (6) NOT NULL
+	subcategory_id VARCHAR (6) NOT NULL,
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+ 	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id) 
 );
 
-CREATE TABLE category (
-	category_id INT PRIMARY KEY,
-	category_name VARCHAR (15)
-);
+-- To disable constraints on the child table (campaign)
+ALTER TABLE campaign DISABLE TRIGGER ALL
 
-CREATE TABLE subcategory (
-	subcategory_id INT PRIMARY KEY,
-	subcategory_name VARCHAR (25)
-);
+-- Insert data into the tables
+-- Import data from category.csv
+" "\\copy public.campaign (cf_id, contact_id, company_name, description, goal, pledged, outcome, backers_count, country, currency, launched_date, end_date, category, subcategory, category_id, subcategory_id) FROM '/Users/marvinalarry/Documents/Education/Tulsa Community College/Data Visualization/Homework/Challenge #13 - Project 2/Crowdfunding_ETL/Resources/campaign.csv' DELIMITER ',' CSV HEADER QUOTE '\"' ESCAPE '''';""
 
-CREATE TABLE contacts (
-	contact_id INT PRIMARY KEY,
-	first_name CHAR (100),
-	last_name CHAR (100),
-	email VARCHAR (255)
-);
+
+-- Import data from subcategory.csv
+
+-- Import data from contacts.csv
+
+-- Import data from campaign.csv
+
+
 
 -- View table columns and datatypes
 SELECT * FROM campaign;
@@ -51,8 +71,9 @@ SELECT * FROM subcategory;
 SELECT * FROM contacts;
 
 -- Insert data into the tables
--- View the tables to ensure all data has been imported correctly
+
 -- Import data from campaign.csv
+
 
 -- Import data from category.csv
 
@@ -60,9 +81,9 @@ SELECT * FROM contacts;
 
 -- Import data from contacts.csv
 
+-- View the tables to ensure all data has been imported correctly
 -- View table columns and datatypes
 SELECT * FROM campaign;
 SELECT * FROM category;
 SELECT * FROM subcategory;
 SELECT * FROM contacts;
-
